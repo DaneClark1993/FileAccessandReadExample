ad07df5fd31b445f091906bd89b52dc71ce0bfc3
daneclark <alphamoX1>
2021-07-23 09:06:33 +1000

    Program using examples of accesing a file and reading the lines contained within until the end of the file is reached, in which case the program will close the file.

---
+++ src/com/company/Main.java
@@ -0,0 +1,37 @@
+package com.company;
+
+import java.io.*;
+import java.util.Scanner;
+
+/************************************
+ * This program was designed to     *
+ * access a file and read the first *
+ * line from it.                    *
+ * It also contains an example of   *
+ * using the hasNext() method to    *
+ * be able to determine whether a   *
+ * file has reached the end of its  *
+ * usable data.                     *
+ * *********************************/
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        Scanner keyboard = new Scanner(System.in);
+
+        System.out.println("Enter the name of a file: ");
+        String filename = keyboard.nextLine();
+
+        File file = new File(filename);
+        Scanner inputFile = new Scanner(file);
+
+        while (inputFile.hasNext()) {
+            String friendName = inputFile.nextLine();
+            System.out.println(friendName);
+        }
+
+        inputFile.close();
+
+    }
+}
